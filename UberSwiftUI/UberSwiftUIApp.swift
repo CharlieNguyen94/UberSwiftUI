72a2c2f5d5ec221f2ee67da76041ffc6078ac958
Charlie <charlietunguyen@hotmail.com>
2023-06-01 16:26:34 +0100

    Initial Commit

---
+++ UberSwiftUI/UberSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberSwiftUIApp.swift
+//  UberSwiftUI
+//
+//  Created by Charlie Nguyen on 01/06/2023.
+//
+
+import SwiftUI
+
+@main
+struct UberSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
